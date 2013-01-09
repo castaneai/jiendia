d98455d39e22cceb223a6abeef9329bb7fa082f1
castaneai <castaneai@castaneai.net>
2013-01-09 23:50:16 +0900

    アーカイブベースを作成

---
+++ src/jiendia/io/_base.py
@@ -0,0 +1,39 @@
+# -*- coding: utf8 -*-
+class ArchiveMode(object):
+    READ = 0
+    CREATE = 1
+    UPDATE = 2
+
+class BaseArchive(object):
+    
+    def __init__(self, stream, mode, encoding):
+        '''アーカイブを開く streamにはファイルのパスかioオブジェクトを渡す'''
+        if isinstance(stream, (str, bytes)):
+            if mode == ArchiveMode.CREATE:
+                open_mode = 'wb'
+            elif mode == ArchiveMode.READ:
+                open_mode = 'rb'
+            elif mode == ArchiveMode.UPDATE:
+                open_mode = 'r+b'
+            stream = open(stream, open_mode)
+        self._stream = stream
+        self._mode = mode
+        self._encoding = encoding
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, *exc):
+        if self._mode in (ArchiveMode.CREATE, ArchiveMode.UPDATE):
+            self.save()
+        
+    def close(self):
+        self._stream.close()
+        
+    def load(self):
+        '''既存のストリームからデータ構造を読み取る'''
+        raise NotImplementedError()
+    
+    def save(self):
+        '''ストリームに変更を保存する'''
+        raise NotImplementedError()
