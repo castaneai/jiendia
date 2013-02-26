a0639d919aa2a40de9b924569204c7a3e99adea2
castaneai <castaneai@castaneai.net>
2013-02-26 17:00:13 +0900

    LDTアーカイブに含まれるテーブルをSQLiteに書き出すテストを作成
    LDTアーカイブは行と列を持つ関係データベースの表形式になっているのでSQLで扱えるのが望ましい。
    
    そういうわけでPythonに標準で装備されているSQLite3のモジュールを使ってSQLiteデータベースにLDTアーカイブの内容を書き出すテストを追加した。

--- src/test/io/ldt_test.py
+++ src/test/io/ldt_test.py
@@ -9,3 +9,12 @@ def test_read():
         for row in ldt.rows:
             pprint(row)
         assert 0
+        
+def test_sql_export():
+    import sqlite3
+    import jiendia.sql.ldt
+    data_dir = os.path.dirname(__file__) + '/data'
+    with LdtArchive(data_dir + '/ACTION_INTERFACE.LDT', ArchiveMode.READ, 'cp932') as ldt:
+        with sqlite3.connect(data_dir + '/action_interface.sqlite') as conn:
+            jiendia.sql.ldt.ldt2sql('action_interface', ldt, conn)
+            
