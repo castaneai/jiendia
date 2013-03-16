d93dd7fa914ca3807db287350e00fea8a53bde9e
castaneai <castaneai@castaneai.net>
2013-03-16 15:01:24 +0900

    アーカイブ自動判別関連処理を独立させた
    今までは__init__.pyの中に直接自動判別関連の関数を入れていたが、importが循環してしまう可能性があることから別モジュールopenに独立させた。

--- src/jiendia/io/__init__.py
+++ src/jiendia/io/__init__.py
@@ -1,17 +1,3 @@
 # -*- coding: utf8 -*-
-import os.path
 from jiendia.io._base import ArchiveMode
-from jiendia.io.spf import SpfArchive
-from jiendia.io.tbl import TblArchive
-from jiendia.io.seq import SeqArchive
-
-def open_archive(path, mode = ArchiveMode.READ, encoding = 'ascii'):
-    ext = os.path.splitext(path)[1].lower()
-    if ext == '.spf':
-        return SpfArchive(path, mode, encoding)
-    if ext == '.tbl':
-        return TblArchive(path, mode, encoding)
-    if ext == '.seq':
-        return SeqArchive(path, mode, encoding)
-    else:
-        raise RuntimeError('Unknown extension {0}'.format(ext))
+from jiendia.io.open import open_archive
---
+++ src/jiendia/io/open.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+import os.path
+from jiendia.io._base import BaseArchive, DEFAULT_MODE, DEFAULT_ENCODING
+from jiendia.io.spf import SpfArchive
+from jiendia.io.tbl import TblArchive
+from jiendia.io.seq import SeqArchive
+from jiendia.io.ldt import LdtArchive
+
+def get_archive_type(filename) -> type:
+    ext = os.path.splitext(filename)[1].lower()
+    if ext == '.spf':
+        return SpfArchive
+    elif ext == '.tbl':
+        return TblArchive
+    elif ext == '.seq':
+        return SeqArchive
+    elif ext == '.ldt':
+        return LdtArchive
+    else:
+        raise RuntimeError('Unknown file extension {0}'.format(ext))
+
+def open_archive(path, mode = DEFAULT_MODE, encoding = DEFAULT_ENCODING) -> BaseArchive:
+    return get_archive_type(path)(path, mode, encoding)
